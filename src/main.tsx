ae675c32f95156ca2f411fe92b9fab4a094bd005
alialfakih01 <ali.fakih@rentallsoftware.com>
2026-05-11 14:59:25 +0300

    added posthog integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import posthog from 'posthog-js'
 import App from './App'
 import './index.css'
+
+posthog.init('phc_kje6cksDDS5tXguUWDATyq5BigTsipVfBUB8ZJ5nnvB9', {
+  api_host: 'https://eu.i.posthog.com',
+  defaults: '2026-01-30',
+  person_profiles: 'identified_only',
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
